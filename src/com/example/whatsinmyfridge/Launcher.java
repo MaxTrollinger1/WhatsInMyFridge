4273f361f9ba07f93505f6138aeea4039c34ab85
Max Trollinger <61511594+MaxTrollinger1@users.noreply.github.com>
2025-05-05 14:07:31 -0400

    Added Launcher

---
+++ src/com/example/whatsinmyfridge/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.whatsinmyfridge;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(WhatsInMyFridgeApp.class, args);
+    }
+}
